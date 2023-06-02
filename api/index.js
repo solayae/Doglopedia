100d1a1338d847a1771241a6052981b1fca06d04
Sebas Olaya <sebastianolayae@gmail.com>
2023-06-01 19:19:08 -0500

    deploy backend railway

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+require('dotenv').config();
+const { PORT } = process.env;
 
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); 
+  server.listen(PORT, () => {
+    console.log('%s listening at', PORT);
   });
-}); 
+});
